4476b92272cea806d76fb6cafc7a1d26ca5b24b5
Sergey Tarsenko <67330524+TatisWeb@users.noreply.github.com>
2020-11-15 19:18:37 +0400

    Add files via upload

--- Newland.js
+++ Newland.js
@@ -1,46 +1,35 @@
-$('a.open-modal').click(function(event) {
-  $(this).modal({
-    fadeDuration: 250
-  });
-  return false;
-});
-
-$("#custom-close").modal({
-  closeClass: 'icon-remove',
-  closeText: '!'
-});
-
 $('.menu-btn').on('click', function(e) {
   e.preventDefault();
   $('.menu').toggleClass('menu_active');
   $('.content').toggleClass('content_active');
 });
 
+$('.btn-bay').on('click', function(e) {
+ e.preventDefault();
+ $('.modal-s').toggleClass('show');
+});
+
 $('.menu-btn').on('click', function(m) {
-  m.preventDefault();
-  $(this).toggleClass('menu-btn_active');
-});
-  
-
-$(document).ready(function() {
-  
-  $("#fade").modal({
-  fadeDuration: 100,
-  fadeDelay: 0.50
-  
+ m.preventDefault();
+ $(this).toggleClass('menu-btn_active');
 });
 
+//$('.btn-bay').click ( function() 
+//  $('.mainfoto').removeClass('wow zoomIn');
+ //   $('p').removeClass('wow zoomInUp');
+ //   new WOW().init();
+   // $(this).toggleClass('wow hinge ');
 
   
-  
-  $('.btn-bay').click ( function() {
-    
-    $('.mainfoto').removeClass('wow zoomIn');
-    $('p').removeClass('wow zoomInUp');
-    new WOW().init();
-    $(this).toggleClass('wow hinge ');
+//});
 
-  
+
+$('.modal-btn').on('click', function(e) {
+  e.preventDefault();
+  $('.modal-s').removeClass('show');
+  //$('.body').removeClass('no-scroll');
 });
-});
+
+
+
 
